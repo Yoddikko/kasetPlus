tsibog/kaset

fix(ai): restore discovery commands and macOS 27 compatibility (#391)
Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

Tests/KasetTests/AIErrorHandlerMacOS27CompatibilityTests.swift
@@ -1,0 +1,121 @@
+import Foundation
+import FoundationModels
+import Testing
+@testable import Kaset
+
+#if compiler(>=6.4)
+    private enum ExpectedAIError: Equatable {
+        case contextWindowExceeded
+        case decodingFailure
+        case contentBlocked
+        case cancelled
+        case notAvailable
+        case modelNotReady
+        case sessionBusy
+        case timedOut
+        case unknown
+    }
+
+    @available(macOS 27.0, *)
+    private func mappedError(_ error: any Error) -> ExpectedAIError {
+        switch AIErrorHandler.handle(error) {
+        case .contextWindowExceeded:
+            .contextWindowExceeded
+        case .decodingFailure:
+            .decodingFailure
+        case .contentBlocked:
+            .contentBlocked
+        case .cancelled:
+            .cancelled
+        case .notAvailable:
+            .notAvailable
+        case .modelNotReady:
+            .modelNotReady
+        case .sessionBusy:
+            .sessionBusy
+        case .timedOut:
+            .timedOut
+        case .unknown:
+            .unknown
+        }
+    }
+
+    @Suite(.tags(.api))
+    struct AIErrorHandlerMacOS27CompatibilityTests {
+        @Test("Maps macOS 27 language model errors")
+        func mapsLanguageModelErrors() {
+            guard #available(macOS 27.0, *) else { return }
+
+            #expect(mappedError(LanguageModelError.contextSizeExceeded(.init(
+                contextSize: 4096,
+                tokenCount: 5000,
+                debugDescription: "test"
+            ))) == .contextWindowExceeded)
+            #expect(mappedError(LanguageModelError.rateLimited(.init(
+                resetDate: nil,
+                debugDescription: "test"
+            ))) == .sessionBusy)
+            #expect(mappedError(LanguageModelError.guardrailViolation(.init(
+                debugDescription: "test"
+            ))) == .contentBlocked)
+            #expect(mappedError(LanguageModelError.refusal(.init(
+                explanation: "test",
+                debugDescription: "test"
+            ))) == .contentBlocked)
+            #expect(mappedError(LanguageModelError.unsupportedLanguageOrLocale(.init(
+                languageCode: .english,
+                debugDescription: "test"
+            ))) == .notAvailable)
+            #expect(mappedError(LanguageModelError.timeout(.init(
+                debugDescription: "test"
+            ))) == .timedOut)
+        }
+
+        @Test("Maps macOS 27 parsing errors")
+        func mapsParsingError() {
+            guard #available(macOS 27.0, *) else { return }
+
+            let error = GeneratedContent.ParsingError(
+                rawContent: "{}",
+                debugDescription: "test"
+            )
+
+            #expect(mappedError(error) == .decodingFailure)
+        }
+
+        @Test("Maps macOS 27 session errors")
+        func mapsSessionErrors() {
+            guard #available(macOS 27.0, *) else { return }
+
+            #expect(mappedError(LanguageModelSession.Error.concurrentRequests) == .sessionBusy)
+            #expect(mappedError(LanguageModelSession.Error.transcriptMutationWhileResponding) == .unknown)
+        }
+
+        @Test("Maps macOS 27 model asset errors")
+        func mapsAssetsUnavailable() {
+            guard #available(macOS 27.0, *) else { return }
+
+            let error = SystemLanguageModel.Error.assetsUnavailable(.init(debugDescription: "test"))
+
+            #expect(mappedError(error) == .notAvailable)
+        }
+
+        @Test("Keeps unsupported macOS 27 features as unknown errors")
+        func keepsUnsupportedFeaturesUnknown() {
+            guard #available(macOS 27.0, *) else { return }
+
+            #expect(mappedError(LanguageModelError.unsupportedCapability(.init(
+                capability: .vision,
+                debugDescription: "test"
+            ))) == .unknown)
+            #expect(mappedError(LanguageModelError.unsupportedTranscriptContent(.init(
+                unsupportedContent: [],
+                debugDescription: "test"
+            ))) == .unknown)
+            #expect(mappedError(LanguageModelError.unsupportedGenerationGuide(.init(
+                schemaName: "TestSchema",
+                debugDescription: "test"
+            ))) == .unknown)
+        }
+    }
+#endif

Tests/KasetTests/CommandBarDiscoveryFailureTests.swift
@@ -1,0 +1,75 @@
+import Foundation
+import Testing
+@testable import Kaset
+
+@Suite(.serialized)
+@MainActor
+struct CommandBarDiscoveryFailureTests {
+    @Test("Curated candidate failures do not fan out across categories")
+    func curatedFailureStopsCandidateTraversal() async {
+        guard #available(macOS 26.0, *) else { return }
+
+        let client = MockYTMusicClient()
+        let player = MockPlayerService()
+        let first = Self.makePlaylist(
+            id: "FEmusic_moods_and_genres_category_first-params",
+            title: "Chill"
+        )
+        let second = Self.makePlaylist(
+            id: "FEmusic_moods_and_genres_category_second-params",
+            title: "Chilled"
+        )
+        client.moodsAndGenresResponse = HomeResponse(sections: [
+            HomeSection(id: "moods", title: "Moods", items: [.playlist(first), .playlist(second)]),
+        ])
+        client.moodCategoryError = YTMusicError.networkError(underlying: URLError(.notConnectedToInternet))
+        client.songsSearchResponse = SearchResponse(
+            songs: [Self.makeSong(title: "Fallback", videoId: "fallback")],
+            albums: [],
+            artists: [],
+            playlists: [],
+            continuationToken: nil
+        )
+
+        _ = await CommandExecutor(client: client, playerService: player).execute(
+            .musicIntent(Self.chillIntent(), originalQuery: "play chill music")
+        )
+
+        #expect(client.moodCategoryParams == ["first-params"])
+        #expect(client.searchQueries == ["chill music"])
+    }
+
+    @available(macOS 26.0, *)
+    private static func chillIntent() -> MusicIntent {
+        MusicIntent(
+            action: .play,
+            query: "chill music",
+            shuffleScope: "",
+            artist: "",
+            genre: "",
+            mood: "chill",
+            era: "",
+            version: "",
+            activity: ""
+        )
+    }
+
+    private static func makePlaylist(id: String, title: String) -> Playlist {
+        Playlist(
+            id: id,
+            title: title,
+            description: nil,
+            thumbnailURL: nil,
+            trackCount: nil
+        )
+    }
+
+    private static func makeSong(title: String, videoId: String) -> Song {
+        Song(
+            id: videoId,
+            title: title,
+            artists: [Artist.inline(name: "Test Artist", namespace: "command-bar-failure-test")],
+            videoId: videoId
+        )
+    }
+}

Tests/KasetTests/CommandBarDiscoveryRegressionTests.swift
@@ -1,0 +1,117 @@
+import Testing
+@testable import Kaset
+
+@Suite(.serialized)
+@MainActor
+struct CommandBarDiscoveryRegressionTests {
+    @Test("Activity cleanup removes grounded aliases")
+    func activityCleanupRemovesGroundedAliases() {
+        guard #available(macOS 26.0, *) else { return }
+
+        let cases = [
+            (query: "jazz music to work out", activity: "workout", expected: "jazz workout songs"),
+            (query: "jazz music to workout", activity: "workout", expected: "jazz workout songs"),
+            (query: "jazz music for workouts", activity: "workout", expected: "jazz workout songs"),
+            (query: "jazz music to code", activity: "focus", expected: "jazz focus songs"),
+            (query: "jazz music while focusing", activity: "focus", expected: "jazz focus songs"),
+            (query: "jazz music to drive", activity: "driving", expected: "jazz driving songs"),
+            (query: "jazz music to cook", activity: "cooking", expected: "jazz cooking songs"),
+        ]
+
+        for item in cases {
+            let intent = MusicIntent(
+                action: .play,
+                query: item.query,
+                shuffleScope: "",
+                artist: "",
+                genre: "jazz",
+                mood: "",
+                era: "",
+                version: "",
+                activity: item.activity
+            )
+
+            #expect(intent.buildSearchQuery() == item.expected)
+        }
+    }
+
+    @Test("Self-titled songs preserve both title and artist roles")
+    func selfTitledSongPreservesTitleAndArtist() {
+        guard #available(macOS 26.0, *) else { return }
+
+        let intent = MusicIntent(
+            action: .play,
+            query: "Black Sabbath by Black Sabbath",
+            shuffleScope: "",
+            artist: "Black Sabbath",
+            genre: "",
+            mood: "",
+            era: "",
+            version: "",
+            activity: ""
+        )
+        #expect(
+            ContentSourceResolver.buildSearchQuery(
+                from: intent,
+                groundingQuery: "play Black Sabbath by Black Sabbath"
+            ) == "Black Sabbath black sabbath songs"
+        )
+    }
+
+    @Test("Activity evidence must come from recognized context")
+    func activityEvidenceIsContextBound() {
+        guard #available(macOS 26.0, *) else { return }
+
+        let collisions = [
+            (query: "Drive", activity: "driving", original: "play Drive while studying"),
+            (query: "Drive", activity: "driving", original: "play Drive and study music"),
+            (query: "Drive", activity: "driving", original: "while studying play Drive"),
+            (query: "Drive", activity: "driving", original: "play Drive with music to relax"),
+            (query: "Born to Run", activity: "running", original: "while studying play Born to Run"),
+        ]
+
+        for item in collisions {
+            let intent = MusicIntent(
+                action: .play,
+                query: item.query,
+                shuffleScope: "",
+                artist: "",
+                genre: "",
+                mood: "",
+                era: "",
+                version: "",
+                activity: item.activity
+            )
+            let grounded = ContentSourceResolver.groundedIntent(intent, groundingQuery: item.original)
+
+            #expect(grounded.activity.isEmpty)
+            #expect(ContentSourceResolver.suggestedContentSource(for: intent, groundingQuery: item.original) == .search)
+            #expect(ContentSourceResolver.buildSearchQuery(from: intent, groundingQuery: item.original) == item.query)
+        }
+
+        let positiveCases = [
+            (activity: "driving", original: "play music for commuting"),
+            (activity: "driving", original: "play music to drive"),
+            (activity: "driving", original: "play driving music"),
+            (activity: "workout", original: "play music for a workout"),
+        ]
+
+        for item in positiveCases {
+            let intent = MusicIntent(
+                action: .play,
+                query: "music",
+                shuffleScope: "",
+                artist: "",
+                genre: "",
+                mood: "",
+                era: "",
+                version: "",
+                activity: item.activity
+            )
+            let grounded = ContentSourceResolver.groundedIntent(intent, groundingQuery: item.original)
+
+            #expect(grounded.activity == item.activity)
+            #expect(ContentSourceResolver.suggestedContentSource(for: intent, groundingQuery: item.original) == .moodsAndGenres)
+        }
+    }
+}

Yoddikko/kasetPlus

Tests/KasetTests/AIErrorHandlerMacOS27CompatibilityTests.swift
@@ -1,0 +1,121 @@
+import Foundation
+import FoundationModels
+import Testing
+@testable import KasetPlus
+
+#if compiler(>=6.4)
+    private enum ExpectedAIError: Equatable {
+        case contextWindowExceeded
+        case decodingFailure
+        case contentBlocked
+        case cancelled
+        case notAvailable
+        case modelNotReady
+        case sessionBusy
+        case timedOut
+        case unknown
+    }
+
+    @available(macOS 27.0, *)
+    private func mappedError(_ error: any Error) -> ExpectedAIError {
+        switch AIErrorHandler.handle(error) {
+        case .contextWindowExceeded:
+            .contextWindowExceeded
+        case .decodingFailure:
+            .decodingFailure
+        case .contentBlocked:
+            .contentBlocked
+        case .cancelled:
+            .cancelled
+        case .notAvailable:
+            .notAvailable
+        case .modelNotReady:
+            .modelNotReady
+        case .sessionBusy:
+            .sessionBusy
+        case .timedOut:
+            .timedOut
+        case .unknown:
+            .unknown
+        }
+    }
+
+    @Suite(.tags(.api))
+    struct AIErrorHandlerMacOS27CompatibilityTests {
+        @Test("Maps macOS 27 language model errors")
+        func mapsLanguageModelErrors() {
+            guard #available(macOS 27.0, *) else { return }
+
+            #expect(mappedError(LanguageModelError.contextSizeExceeded(.init(
+                contextSize: 4096,
+                tokenCount: 5000,
+                debugDescription: "test"
+            ))) == .contextWindowExceeded)
+            #expect(mappedError(LanguageModelError.rateLimited(.init(
+                resetDate: nil,
+                debugDescription: "test"
+            ))) == .sessionBusy)
+            #expect(mappedError(LanguageModelError.guardrailViolation(.init(
+                debugDescription: "test"
+            ))) == .contentBlocked)
+            #expect(mappedError(LanguageModelError.refusal(.init(
+                explanation: "test",
+                debugDescription: "test"
+            ))) == .contentBlocked)
+            #expect(mappedError(LanguageModelError.unsupportedLanguageOrLocale(.init(
+                languageCode: .english,
+                debugDescription: "test"
+            ))) == .notAvailable)
+            #expect(mappedError(LanguageModelError.timeout(.init(
+                debugDescription: "test"
+            ))) == .timedOut)
+        }
+
+        @Test("Maps macOS 27 parsing errors")
+        func mapsParsingError() {
+            guard #available(macOS 27.0, *) else { return }
+
+            let error = GeneratedContent.ParsingError(
+                rawContent: "{}",
+                debugDescription: "test"
+            )
+
+            #expect(mappedError(error) == .decodingFailure)
+        }
+
+        @Test("Maps macOS 27 session errors")
+        func mapsSessionErrors() {
+            guard #available(macOS 27.0, *) else { return }
+
+            #expect(mappedError(LanguageModelSession.Error.concurrentRequests) == .sessionBusy)
+            #expect(mappedError(LanguageModelSession.Error.transcriptMutationWhileResponding) == .unknown)
+        }
+
+        @Test("Maps macOS 27 model asset errors")
+        func mapsAssetsUnavailable() {
+            guard #available(macOS 27.0, *) else { return }
+
+            let error = SystemLanguageModel.Error.assetsUnavailable(.init(debugDescription: "test"))
+
+            #expect(mappedError(error) == .notAvailable)
+        }
+
+        @Test("Keeps unsupported macOS 27 features as unknown errors")
+        func keepsUnsupportedFeaturesUnknown() {
+            guard #available(macOS 27.0, *) else { return }
+
+            #expect(mappedError(LanguageModelError.unsupportedCapability(.init(
+                capability: .vision,
+                debugDescription: "test"
+            ))) == .unknown)
+            #expect(mappedError(LanguageModelError.unsupportedTranscriptContent(.init(
+                unsupportedContent: [],
+                debugDescription: "test"
+            ))) == .unknown)
+            #expect(mappedError(LanguageModelError.unsupportedGenerationGuide(.init(
+                schemaName: "TestSchema",
+                debugDescription: "test"
+            ))) == .unknown)
+        }
+    }
+#endif

Tests/KasetTests/CommandBarDiscoveryFailureTests.swift
@@ -1,0 +1,75 @@
+import Foundation
+import Testing
+@testable import KasetPlus
+
+@Suite(.serialized)
+@MainActor
+struct CommandBarDiscoveryFailureTests {
+    @Test("Curated candidate failures do not fan out across categories")
+    func curatedFailureStopsCandidateTraversal() async {
+        guard #available(macOS 26.0, *) else { return }
+
+        let client = MockYTMusicClient()
+        let player = MockPlayerService()
+        let first = Self.makePlaylist(
+            id: "FEmusic_moods_and_genres_category_first-params",
+            title: "Chill"
+        )
+        let second = Self.makePlaylist(
+            id: "FEmusic_moods_and_genres_category_second-params",
+            title: "Chilled"
+        )
+        client.moodsAndGenresResponse = HomeResponse(sections: [
+            HomeSection(id: "moods", title: "Moods", items: [.playlist(first), .playlist(second)]),
+        ])
+        client.moodCategoryError = YTMusicError.networkError(underlying: URLError(.notConnectedToInternet))
+        client.songsSearchResponse = SearchResponse(
+            songs: [Self.makeSong(title: "Fallback", videoId: "fallback")],
+            albums: [],
+            artists: [],
+            playlists: [],
+            continuationToken: nil
+        )
+
+        _ = await CommandExecutor(client: client, playerService: player).execute(
+            .musicIntent(Self.chillIntent(), originalQuery: "play chill music")
+        )
+
+        #expect(client.moodCategoryParams == ["first-params"])
+        #expect(client.searchQueries == ["chill music"])
+    }
+
+    @available(macOS 26.0, *)
+    private static func chillIntent() -> MusicIntent {
+        MusicIntent(
+            action: .play,
+            query: "chill music",
+            shuffleScope: "",
+            artist: "",
+            genre: "",
+            mood: "chill",
+            era: "",
+            version: "",
+            activity: ""
+        )
+    }
+
+    private static func makePlaylist(id: String, title: String) -> Playlist {
+        Playlist(
+            id: id,
+            title: title,
+            description: nil,
+            thumbnailURL: nil,
+            trackCount: nil
+        )
+    }
+
+    private static func makeSong(title: String, videoId: String) -> Song {
+        Song(
+            id: videoId,
+            title: title,
+            artists: [Artist.inline(name: "Test Artist", namespace: "command-bar-failure-test")],
+            videoId: videoId
+        )
+    }
+}

Tests/KasetTests/CommandBarDiscoveryRegressionTests.swift
@@ -1,0 +1,117 @@
+import Testing
+@testable import KasetPlus
+
+@Suite(.serialized)
+@MainActor
+struct CommandBarDiscoveryRegressionTests {
+    @Test("Activity cleanup removes grounded aliases")
+    func activityCleanupRemovesGroundedAliases() {
+        guard #available(macOS 26.0, *) else { return }
+
+        let cases = [
+            (query: "jazz music to work out", activity: "workout", expected: "jazz workout songs"),
+            (query: "jazz music to workout", activity: "workout", expected: "jazz workout songs"),
+            (query: "jazz music for workouts", activity: "workout", expected: "jazz workout songs"),
+            (query: "jazz music to code", activity: "focus", expected: "jazz focus songs"),
+            (query: "jazz music while focusing", activity: "focus", expected: "jazz focus songs"),
+            (query: "jazz music to drive", activity: "driving", expected: "jazz driving songs"),
+            (query: "jazz music to cook", activity: "cooking", expected: "jazz cooking songs"),
+        ]
+
+        for item in cases {
+            let intent = MusicIntent(
+                action: .play,
+                query: item.query,
+                shuffleScope: "",
+                artist: "",
+                genre: "jazz",
+                mood: "",
+                era: "",
+                version: "",
+                activity: item.activity
+            )
+
+            #expect(intent.buildSearchQuery() == item.expected)
+        }
+    }
+
+    @Test("Self-titled songs preserve both title and artist roles")
+    func selfTitledSongPreservesTitleAndArtist() {
+        guard #available(macOS 26.0, *) else { return }
+
+        let intent = MusicIntent(
+            action: .play,
+            query: "Black Sabbath by Black Sabbath",
+            shuffleScope: "",
+            artist: "Black Sabbath",
+            genre: "",
+            mood: "",
+            era: "",
+            version: "",
+            activity: ""
+        )
+        #expect(
+            ContentSourceResolver.buildSearchQuery(
+                from: intent,
+                groundingQuery: "play Black Sabbath by Black Sabbath"
+            ) == "Black Sabbath black sabbath songs"
+        )
+    }
+
+    @Test("Activity evidence must come from recognized context")
+    func activityEvidenceIsContextBound() {
+        guard #available(macOS 26.0, *) else { return }
+
+        let collisions = [
+            (query: "Drive", activity: "driving", original: "play Drive while studying"),
+            (query: "Drive", activity: "driving", original: "play Drive and study music"),
+            (query: "Drive", activity: "driving", original: "while studying play Drive"),
+            (query: "Drive", activity: "driving", original: "play Drive with music to relax"),
+            (query: "Born to Run", activity: "running", original: "while studying play Born to Run"),
+        ]
+
+        for item in collisions {
+            let intent = MusicIntent(
+                action: .play,
+                query: item.query,
+                shuffleScope: "",
+                artist: "",
+                genre: "",
+                mood: "",
+                era: "",
+                version: "",
+                activity: item.activity
+            )
+            let grounded = ContentSourceResolver.groundedIntent(intent, groundingQuery: item.original)
+
+            #expect(grounded.activity.isEmpty)
+            #expect(ContentSourceResolver.suggestedContentSource(for: intent, groundingQuery: item.original) == .search)
+            #expect(ContentSourceResolver.buildSearchQuery(from: intent, groundingQuery: item.original) == item.query)
+        }
+
+        let positiveCases = [
+            (activity: "driving", original: "play music for commuting"),
+            (activity: "driving", original: "play music to drive"),
+            (activity: "driving", original: "play driving music"),
+            (activity: "workout", original: "play music for a workout"),
+        ]
+
+        for item in positiveCases {
+            let intent = MusicIntent(
+                action: .play,
+                query: "music",
+                shuffleScope: "",
+                artist: "",
+                genre: "",
+                mood: "",
+                era: "",
+                version: "",
+                activity: item.activity
+            )
+            let grounded = ContentSourceResolver.groundedIntent(intent, groundingQuery: item.original)
+
+            #expect(grounded.activity == item.activity)
+            #expect(ContentSourceResolver.suggestedContentSource(for: intent, groundingQuery: item.original) == .moodsAndGenres)
+        }
+    }
+}